aea5d10062df11e33627a251a1b0f9e4bbe5bffc
John Lyold Lozada <lozadajohnlyoldcastro12705@gmail.com>
2026-06-13 00:42:23 +0800

    feat: replace floating PC with unified Sad Office Room model

--- src/components/public/crt-os-scene.tsx
+++ src/components/public/crt-os-scene.tsx
@@ -1,32 +1,32 @@
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Environment, PerspectiveCamera } from '@react-three/drei';
-import { Model as IbmPc } from './ibm-pc';
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { SadOfficeRoom } from './sad-office-room';
 
 export function CRTOsScene({ isBootingOS, onCompleteBoot }: { isBootingOS: boolean, onCompleteBoot: () => void }) {
   return (
     <div className="w-full h-full bg-black">
       <Canvas>
-        <PerspectiveCamera makeDefault position={[0, 1.5, 5]} fov={50} />
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[10, 10, 5]} intensity={1} />
+        <PerspectiveCamera makeDefault position={[0, 5, 20]} fov={50} />
+        
+        {/* Lights for the MeshStandardMaterial */}
+        <ambientLight intensity={1.5} />
+        <directionalLight position={[10, 10, 5]} intensity={2} />
         <pointLight position={[-10, -10, -10]} intensity={0.5} />
         
         <Suspense fallback={null}>
-          <group position={[0, -1, 0]}>
-            <IbmPc isBootingOS={isBootingOS} onCompleteBoot={onCompleteBoot} />
+          <group position={[0, -2, 0]}>
+            <SadOfficeRoom isBootingOS={isBootingOS} onCompleteBoot={onCompleteBoot} />
           </group>
-          {/* Default environment to make the beige plastic look good */}
-          <Environment preset="city" />
         </Suspense>
 
-        {/* Let the user look around the physical monitor! */}
+        {/* Camera Limits */}
         <OrbitControls 
           enablePan={false} 
           minPolarAngle={Math.PI / 4}
-          maxPolarAngle={Math.PI / 1.5}
-          minDistance={2}
-          maxDistance={8}
+          maxPolarAngle={Math.PI / 2 + 0.1} // Allow looking down at the keyboard, but not too far under the desk
+          minDistance={5}
+          maxDistance={30}
         />
       </Canvas>
     </div>
